c1d7f3fa0f3e079632f43d6ed1d5bb42d271bc37
Mohammad Nayemul Islam <nayem1010s@gmail.com>
2025-11-12 23:44:49 +0600

    Added Spring Security

--- HospitalMangement/src/main/java/com/practice/HospitalMangement/entity/Patient.java
+++ HospitalMangement/src/main/java/com/practice/HospitalMangement/entity/Patient.java
@@ -44,19 +44,4 @@ public class Patient {
     @ToString.Exclude
     private List<Appointment> appointments;
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Patient patient = (Patient) o;
-        return getId() != null && Objects.equals(getId(), patient.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
